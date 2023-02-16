29f7b49d194a2de30f3ecb8ec7b67478477f1249
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-16 12:47:50 -0300

    build: import arrow and libras

--- src/containers/Inicio.jsx
+++ src/containers/Inicio.jsx
@@ -1,10 +1,12 @@
 import Header from  "../components/Header/Header.jsx"
 import Footer from  "../components/Footer/Footer.jsx"
-import Hero from  "../components/Inicio/Hero.jsx"
-import Sobre from  "../components/Inicio/Sobre.jsx"
-import Cursos from "../components/Inicio/Cursos.jsx"
-import Telegram from "../components/Inicio/Telegram.jsx"
-import Contato from "../components/Inicio/Contato.jsx"
+import Hero from  "../pages/Inicio/Hero.jsx"
+import Sobre from  "../pages/Inicio/Sobre.jsx"
+import Cursos from "../pages/Inicio/Cursos.jsx"
+import Telegram from "../pages/Inicio/Telegram.jsx"
+import Contato from "../pages/Inicio/Contato.jsx"
+import ArrowToTop from "../components/ArrowToTop/ArrowToTop.jsx"
+import Libras from "../components/Libras.jsx"
 
 function Inicio() {
   return (
@@ -16,6 +18,8 @@ function Inicio() {
     <Cursos />
     <Telegram />
     <Contato />
+    <ArrowToTop />
+    <Libras />
 
 
     <Footer />
