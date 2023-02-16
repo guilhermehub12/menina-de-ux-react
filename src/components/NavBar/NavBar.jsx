14e2b4883d6d34b6201e3f36ce411e7369d94fd7
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-16 12:46:10 -0300

    feat: navbar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,43 @@
+import { List, X } from "react-bootstrap-icons";
+import { React, useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
   const pages = ["Início", "Estude", "Cursos", "Bootcamps", "Sobre", "Contato"];
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const handleMobileNavToggle = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const navbarClass = `${isMobileNavOpen ? "navbar-mobile" : ""}`;
+
+  const handleMobileDropdownClick = (e) =>
+    {
+      if(isMobileNavOpen) {
+        e.preventDefault();
+        e.currentTarget.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    };
 
   return (
-    <nav id="navbar" className="navbar">
+    <nav id="navbar" className={navbarClass}>
       <ul>
         {pages.map((page, index) => {
           return (
-            <li key={`${page}-${index}`}>
-              <a className="nav-link scrollto" href="#">
+            <li key={`${page}-${index}`} className="nav-item">
+              <a
+                className="nav-link scrollto"
+                href="#"
+                onClick={handleMobileDropdownClick}
+              >
                 {page}
               </a>
             </li>
-          )
+          );
         })}
       </ul>
-      <i className="bi bi-list mobile-nav-toggle"></i>
+        <List className="mobile-nav-toggle" onClick={handleMobileNavToggle} />
     </nav>
   );
 }
