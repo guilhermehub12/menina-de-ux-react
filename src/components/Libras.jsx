449ade36de49196a101d03f3bfb05be4bc03983e
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-16 12:42:34 -0300

    vlibras component

---
+++ src/components/Libras.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VLibras from "@djpfs/react-vlibras";
+
+function Libras() {
+  return (
+    <div className="App">
+      <VLibras forceOnload={true} />
+    </div>
+  );
+}
+
+export default Libras;
