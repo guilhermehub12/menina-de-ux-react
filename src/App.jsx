9bbf9587c0987bfc510993ba8fe938b58de4f66b
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-16 12:42:23 -0300

    route to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { ArrowUp } from "react-bootstrap-icons";
 import Inicio from "./containers/Inicio";
+import Estude from "./containers/Estude";
+import Cursos from "./containers/Cursos";
+import Bootcamps from "./containers/Bootcamps";
+import Sobre from "./containers/Sobre";
+import Contato from "./containers/Contato";
 import "./App.css";
 
 function App() {
-  const [showButton, setShowButton] = useState(false);
-
-  useEffect(() => {
-    const toggleBackToTop = () => {
-      if (window.scrollY > 100) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    };
-
-    window.addEventListener("scroll", toggleBackToTop);
-
-    return () => {
-      window.removeEventListener("scroll", toggleBackToTop);
-    };
-  }, []);
-
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <div className="App">
       {
         <BrowserRouter>
           <Routes>
-            <Route path="/inicio" element={<Inicio />} />
-            {/* <Route path="/estude" element={<Estude />} />
+            <Route path="/" element={<Inicio />} />
+            <Route path="/estude" element={<Estude />} />
             <Route path="/cursos" element={<Cursos />} />
             <Route path="/bootcamps" element={<Bootcamps />} />
             <Route path="/sobre" element={<Sobre />} />
-            <Route path="/contato" element={<Contato />} /> */}
+            <Route path="/contato" element={<Contato />} />
           </Routes>
         </BrowserRouter>
       }
-
-      <a
-        onClick={handleClick}
-        className={`back-to-top ${showButton ? 'active' : ''} d-flex align-items-center justify-content-center`}
-      >
-        <ArrowUp color={"#FFF"} />
-      </a>
     </div>
   );
 }
