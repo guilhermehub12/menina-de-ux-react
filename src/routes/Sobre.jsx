5a2239128182bb4d5f7bd48be58308695afe9118
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:41:47 -0300

    routes: Sobre page

---
+++ src/routes/Sobre.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Hero from "../pages/Sobre/Hero.jsx";
+
+function Sobre() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default Sobre;
