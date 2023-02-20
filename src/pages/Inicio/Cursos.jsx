eaa636a26a204d26c118110343c84936e5528622
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:37:27 -0300

    build: Cursos section to Inicio page
    Renders a section on a webpage displaying a list of courses and the latest selection processes related to those courses. The component imports various modules, such as React, react-bootstrap, react-bootstrap-icons, and react-router-dom, and also imports a local stylesheet. The component uses map() to loop through an array of course and selection process objects and renders them as cards using HTML and CSS. Finally, the component returns the complete section with the course and selection process cards, and a button to view more courses and processes.

--- src/pages/Inicio/Cursos.jsx
+++ src/pages/Inicio/Cursos.jsx
@@ -1,36 +1,67 @@
 import React from "react";
+import { Row } from "react-bootstrap";
+import { ArrowRight } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 import "./Cursos.css";
 
 function Cursos() {
-  const cursos = [
-    {
-      curso: "Curso em Vídeo",
-      alternativeText: "Curso em Vídeo",
-      src: "../src/assets/img/cursos/cursoemvideo.webp",
-      href: "https://www.cursoemvideo.com/cursos/",
-    },
-    {
-      curso: "Digital Innovation One",
-      alternativeText: "Digital Innovation One",
-      src: "../src/assets/img/cursos/dio.webp",
-      href: "https://dio.me/sign-up?ref=U5U6LS080B",
-    },
-    {
-      curso: "Estação Hack",
-      alternativeText: "Estação Hack",
-      src: "../src/assets/img/cursos/estacaohack.webp",
-      href: "https://br.cellep.com/estacao-hack-sp/",
-    },
-    {
-      curso: "Mundo SENAI",
-      alternativeText: "Mundo SENAI",
-      src: "../src/assets/img/cursos/mundosenai.webp",
-      href: "https://loja.mundosenai.com.br",
-    },
-  ];
-
+  const posts = {
+    cursos: [
+      {
+        curso: "Curso em Vídeo",
+        alternativeText: "Curso em Vídeo",
+        src: "../src/assets/img/cursos/cursoemvideo.webp",
+        href: "https://www.cursoemvideo.com/cursos/",
+      },
+      {
+        curso: "Digital Innovation One",
+        alternativeText: "Digital Innovation One",
+        src: "../src/assets/img/cursos/dio.webp",
+        href: "https://dio.me/sign-up?ref=U5U6LS080B",
+      },
+      {
+        curso: "Estação Hack",
+        alternativeText: "Estação Hack",
+        src: "../src/assets/img/cursos/estacaohack.webp",
+        href: "https://br.cellep.com/estacao-hack-sp/",
+      },
+      {
+        curso: "Mundo SENAI",
+        alternativeText: "Mundo SENAI",
+        src: "../src/assets/img/cursos/mundosenai.webp",
+        href: "https://loja.mundosenai.com.br",
+      },
+    ],
+    processoSeletivo: [
+      {
+        src: "../src/assets/img/bootcamps/bootcampinhouiux.webp",
+        alt: "Bootcampinho UI/UX",
+        titulo: "Bootcampinho UI/UX",
+        href: "https://maiane.com.br/bootcampinho-ui-ux-design/",
+      },
+      {
+        src: "../src/assets/img/bootcamps/proztecnologia.webp",
+        alt: "Capacitação Gratuita em Introdução à Programação",
+        titulo: "Capacitação Gratuita em Introdução à Programação",
+        subTitulo: "Inscrições por tempo limitado!",
+        href: "https://pages.prozeducacao.com.br/proz-tecnologia/",
+      },
+      {
+        src: "../src/assets/img/bootcamps/gamaacademy.webp",
+        alt: "6 cursos grátis da Gama Academy",
+        titulo: "6 cursos grátis da Gama Academy",
+        href: "https://app.gama.academy/jornada/e55b1f-semana-de-rebranding/",
+      },
+      {
+        src: "../src/assets/img/bootcamps/cursosprefeituradebh.webp",
+        alt: "Cursos de TI da Prefeitura de BH",
+        titulo: "Cursos de TI da Prefeitura de BH",
+        href: "https://docs.google.com/forms/d/e/1FAIpQLSc9Q6G2A5yakCrGfUaqUFrIH63WYw4Tguo1MpOmcSittFHasQ/viewform/",
+      },
+    ],
+  };
   return (
-    <section id="cursos" className="cursos">
+    <section id="cursos" className="cursos-inicio">
       <div className="container">
         <div className="section-title" data-aos="fade-up">
           <h2>Cursos Tops</h2>
@@ -39,32 +70,37 @@ function Cursos() {
       </div>
 
       <div className="row justify-content-center">
-        {cursos.map((curso, index) => {
+        {posts.cursos.map((curso, index) => {
           return (
-            <div key={`${curso}-${index}`} className="post-box" data-aos="fade-up">
+            <div
+              key={`${curso}-${index}`}
+              className="post-box"
+              data-aos="fade-up"
+            >
               <div className="post-img">
                 <img
                   src={curso.src}
                   className="img-fluid"
+                  width={"100%"}
                   draggable="false"
                   alt={curso.alternativeText}
                 />
               </div>
               <h3 className="post-title">{curso.curso}</h3>
-              <a
-                href={curso.href}
+              <Link
+                to={curso.href}
                 target="_blank"
                 className="readmore stretched-link mt-auto"
-              ></a>
+              ></Link>
             </div>
           );
         })}
       </div>
 
       <div className="text-center text-lg-start">
-        <a href="cursos.html" className="btn-ver-todos">
+        <Link to="/cursos" className="btn-ver-todos">
           Descobrir mais cursos
-        </a>
+        </Link>
       </div>
 
       <div className="container">
@@ -74,93 +110,37 @@ function Cursos() {
         </div>
       </div>
 
-      <div className="row justify-content-center">
-        <div className="post-box cursos-item filter-design">
-          <div className="post-img">
-            <img
-              src="src/assets/img/bootcamps/bootcampinhouiux.webp"
-              className="img-fluid"
-              draggable="false"
-              alt="Bootcampinho UI/UX"
-            />
-          </div>
-          <h3 className="post-title">Bootcampinho UI/UX</h3>
-          <a
-            href="https://maiane.com.br/bootcampinho-ui-ux-design/"
-            target="_blank"
-            className="readmore stretched-link mt-auto"
-          >
-            <span>Saiba mais</span>
-            <i className="bi bi-arrow-right"></i>
-          </a>
-        </div>
-        <div className="post-box cursos-item filter-back-end">
-          <div className="post-img">
-            <img
-              src="src/assets/img/bootcamps/proztecnologia.webp"
-              className="img-fluid"
-              draggable="false"
-              alt="Capacitação Gratuita em Introdução à Programação"
-            />
-          </div>
-          <h3 className="post-title">
-            Capacitação Gratuita em Introdução à Programação
-          </h3>
-          <p className="post-date">Inscrições por tempo limitado!</p>
-          <a
-            href="https://pages.prozeducacao.com.br/proz-tecnologia"
-            target="_blank"
-            className="readmore stretched-link mt-auto"
-          >
-            <span>Saiba mais</span>
-            <i className="bi bi-arrow-right"></i>
-          </a>
-        </div>
-        <div className="post-box cursos-item filter-active">
-          <div className="post-img">
-            <img
-              src="src/assets/img/bootcamps/gamaacademy.webp"
-              className="img-fluid"
-              draggable="false"
-              alt="6 cursos grátis da Gama Academy"
-            />
-          </div>
-          <h3 className="post-title">6 cursos grátis da Gama Academy</h3>
-          <a
-            href="https://app.gama.academy/jornada/e55b1f-semana-de-rebranding/"
-            target="_blank"
-            className="readmore stretched-link mt-auto"
-          >
-            <span>Saiba mais</span>
-            <i className="bi bi-arrow-right"></i>
-          </a>
-        </div>
-
-        <div className="post-box cursos-item filter-active">
-          <div className="post-img">
-            <img
-              src="src/assets/img/bootcamps/cursosprefeituradebh.webp"
-              className="img-fluid"
-              draggable="false"
-              alt="Cursos de TI da Prefeitura de BH"
-            />
-          </div>
-          <h3 className="post-title">Cursos de TI da Prefeitura de BH</h3>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSc9Q6G2A5yakCrGfUaqUFrIH63WYw4Tguo1MpOmcSittFHasQ/viewform"
-            target="_blank"
-            className="readmore stretched-link mt-auto"
-          >
-            <span>Saiba mais</span>
-            <i className="bi bi-arrow-right"></i>
-          </a>
-        </div>
-      </div>
+      <Row className="justify-content-center">
+        {posts.processoSeletivo.map((processo, index) => {
+          return (
+            <div key={`${processo}-${index}`} className="post-box cursos-item filter-design">
+              <div className="post-img">
+                <img
+                  src={processo.src}
+                  className="img-fluid"
+                  draggable="false"
+                  alt={processo.alt}
+                />
+              </div>
+              <h3 className="post-title">{processo.titulo}</h3>
+              <p className="post-date">{processo.subTitulo}</p>
+              <Link
+                to={processo.href}
+                target="_blank"
+                className="readmore stretched-link mt-auto"
+              >
+                <span>Saiba mais</span>
+                <ArrowRight className="seta" />
+              </Link>
+            </div>
+          );
+        })}
+      </Row>
 
       <div className="text-center text-lg-start">
-        <a href="bootcamps.html" className="btn-ver-todos">
+        <Link to="/bootcamps" className="btn-ver-todos">
           Ver mais
-        </a>
+        </Link>
       </div>
     </section>
   );
