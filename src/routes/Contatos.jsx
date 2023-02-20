2e2630c935d2d944884cdc2ed5063d16f47dee74
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:41:07 -0300

    routes: Contatos page

---
+++ src/routes/Contatos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header/Header.jsx";
+import Telegram from "../components/Telegram/Telegram.jsx";
+import Contato from "../components/Contato/Contato.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+
+function Contatos() {
+  return (
+    <>
+      <Header />
+      <Telegram />
+      <Contato />
+      <Footer />
+    </>
+  );
+}
+
+export default Contatos;
