344d6cadfc433cb735c87e42645a553315de779f
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 22:43:46 -0300

    build: organization and 404 route
    Organizing imports, Aos and Libras to be used in all pages and 404 page when url is undefined

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/global.css";
 import Inicio from "./routes/Inicio";
 import Estude from "./routes/Estude";
 import Cursos from "./routes/Cursos";
 import Bootcamps from "./routes/Bootcamps";
 import Sobre from "./routes/Sobre";
-import Contato from "./routes/Contato";
-import "./styles/global.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import Contatos from "./routes/Contatos";
+import { Aos } from "./utils/Aos";
+import NotFound from "./pages/NotFound/NotFound";
+import Libras from "./utils/Libras";
 
 function App() {
-  AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 1000, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 1000, // the delay on throttle used while scrolling the page (advanced)
-    
-  
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 400, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-  
-  });
   return (
     <>
+      <Aos />
+      <Libras />
       {
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/estude" element={<Estude />} />
             <Route path="/cursos" element={<Cursos />} />
             <Route path="/bootcamps" element={<Bootcamps />} />
             <Route path="/sobre" element={<Sobre />} />
-            <Route path="/contato" element={<Contato />} />
+            <Route path="/contatos" element={<Contatos />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       }
     </>
   );
