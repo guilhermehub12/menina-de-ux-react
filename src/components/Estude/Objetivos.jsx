df853d8f3f8c2b0fed6ca1c038cd92bfd46700f8
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:27:11 -0300

    fix: text wrapping line and quotation marks

--- src/components/Estude/Objetivos.jsx
+++ src/components/Estude/Objetivos.jsx
@@ -8,9 +8,7 @@ function Objetivos() {
       imagemLink: "../src/assets/img/fazer-user-flow.webp",
       alt: "Ilustração de uma menina olhando para trás navegando em um smarthphone grande",
       titulo: "Criando seu Case",
-      descricao: `Com um template no Figma, os posts de guia e os conteúdos
-      que você só encontra com a Menina de UX, você vai
-      conseguir se guiar para construir o seu case do zero!`,
+      descricao: "Com um template no Figma, os posts de guia e os conteúdos que você só encontra com a Menina de UX, você vai conseguir se guiar para construir o seu case do zero!",
       botaoLink: "/criando-seu-case",
       botaoTexto: "Crie seu primeiro projeto!",
     },
@@ -18,9 +16,7 @@ function Objetivos() {
       imagemLink: "../src/assets/img/equipe-designers.webp",
       alt: "Ilustração de três pessoa, o da esquerda segura um lápis grande, a do meio segura um notebook e a terceira segura algo parecido com uma caneta de tinta",
       titulo: "Carreiras em UX",
-      descricao: `Com um template no Figma, os posts de guia e os conteúdos
-      que você só encontra com a Menina de UX, você vai
-      conseguir se guiar para construir o seu case do zero!`,
+      descricao: "Com um template no Figma, os posts de guia e os conteúdos que você só encontra com a Menina de UX, você vai conseguir se guiar para construir o seu case do zero!",
       botaoLink: "/carreiras-em-ux",
       botaoTexto: "Conheça as carreiras!",
     },
