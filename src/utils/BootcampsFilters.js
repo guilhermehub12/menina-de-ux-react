28b7c78708f301a130e66f9ecc3cf6438bfa3f2e
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:46:09 -0300

    build: Bootcamps list of filters

---
+++ src/utils/BootcampsFilters.js
@@ -0,0 +1,39 @@
+export const BootcampsFilters = [
+    {
+      filter: "*",
+      titulo: "Todos",
+    },
+    {
+      filter: ".filter-back-end",
+      titulo: "Back-End",
+    },
+    {
+      filter: ".filter-front-end",
+      titulo: "Front-End",
+    },
+    {
+      filter: ".filter-design",
+      titulo: "Design",
+    },
+    {
+      filter: ".filter-dados",
+      titulo: "Cloud e Dados",
+    },
+    {
+      filter: ".filter-marketing",
+      titulo: "Marketing e Business",
+    },
+    {
+      filter: ".filter-idiomas",
+      titulo: "Idiomas",
+    },
+    {
+      filter: ".filter-mulheres",
+      titulo: "Para Mulheres",
+    },
+    {
+      filter: ".filter-jovens",
+      titulo: "Para Jovens",
+    },
+  ];
+  
