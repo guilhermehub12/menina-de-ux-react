a106dc4ed44dc42ecc1747b0f3f1a9bdbedde360
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:46:36 -0300

    build: Cursos list of filters

---
+++ src/utils/CursosFilters.js
@@ -0,0 +1,38 @@
+export const CursosFilters = [
+  {
+    filter: "*",
+    titulo: "Todos",
+  },
+  {
+    filter: ".filter-cloud",
+    titulo: "Cloud e Data Science",
+  },
+  {
+    filter: ".filter-desenvolvimento",
+    titulo: "Desenvolvimento",
+  },
+  {
+    filter: ".filter-games",
+    titulo: "Games",
+  },
+  {
+    filter: ".filter-marketing",
+    titulo: "Marketing",
+  },
+  {
+    filter: ".filter-idiomas",
+    titulo: "Idiomas",
+  },
+  {
+    filter: ".filter-soft",
+    titulo: "Soft-Skills",
+  },
+  {
+    filter: ".filter-UX-design",
+    titulo: "UI/UX Design",
+  },
+  {
+    filter: ".filter-UX-writing",
+    titulo: "UX Writing",
+  },
+];
