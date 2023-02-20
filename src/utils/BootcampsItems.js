5c7e36096061c6886aed5f4b4188fb0861ae669e
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:46:26 -0300

    build: Bootcamps list of items

---
+++ src/utils/BootcampsItems.js
@@ -0,0 +1,58 @@
+export const BootcampsItems = [
+  {
+    cursoFilter: "design",
+    imageLink: "src/assets/img/bootcamps/bootcampinhouiux.webp",
+    imageAlt: "Bootcampinho UI/UX",
+    postTitle: "Bootcampinho UI/UX",
+    postContent:
+      "O Bootcampinho Gratuito UI/UX Design tem como objetivo te ajudar nos primeiros passos de sua jornada. Formulado com o objetivo de ser um ponto de partida o curso é dividido entre etapas teóricas e práticas. \n\nAlém disso, você será desafiado(a) durante todo o curso a criar um projeto de UI/UX do início ao fim. Todas as aulas também contam com desafios e materiais de para ajudar você a continuar seus estudos.",
+      postLink: "https://maiane.com.br/bootcampinho-ui-ux-design/",
+  },
+  {
+    cursoFilter: "back-end",
+    imageLink: "src/assets/img/bootcamps/proztecnologia.webp",
+    imageAlt: "Capacitação Gratuita em Introdução à Programação",
+    postTitle: "Capacitação Gratuita em Introdução à Programação",
+    postOptional: "Inscrições por tempo limitado!",
+    postContent:
+      "A Proz Tecnologia está oferecendo uma capacitação gratuita em Introdução a Programação. Você não precisa saber programar, mas se souber, vem também! Você vai começar  aprendendo conceitos básicos, e será levado até que possa desenvolver seus próprios projetos.",
+    postLink: "https://pages.prozeducacao.com.br/proz-tecnologia",
+  },
+  {
+    cursoFilter: "active",
+    imageLink: "src/assets/img/bootcamps/gamaacademy.webp",
+    imageAlt: "6 cursos grátis da Gama Academy",
+    postTitle: "6 cursos grátis da Gama Academy",
+    postContent:
+      "A Gama Academy está de cara nova! Enquanto celebram sua nova marca, querem premiar você, que torna isso possível, com acesso gratuito a alguns dos nossos conteúdos mais bem avaliados de Desenvolvimento Web e Product Design. Aproveitem!",
+    postLink: "https://app.gama.academy/jornada/e55b1f-semana-de-rebranding/",
+  },
+  {
+    cursoFilter: "active",
+    imageLink: "src/assets/img/bootcamps/cursosprefeituradebh.webp",
+    imageAlt: "Cursos de TI da Prefeitura de BH",
+    postTitle: "Cursos de TI da Prefeitura de BH",
+    postContent:
+      "A Prefeitura de Belo Horizonte está com mais de 2.500 vagas disponíveis para cursos de inclusão digital nos formatos presencial e EAD para a área de tecnologia da informação. \n\nSão cinco cursos disponíveis, cada um deles com 500 vagas. O aluno pode cursar todos eles, iniciando com Introdução às Novas Tecnologias, Planilha Eletrônica, Ferramentas para Home Office, chegando à Programação Web, sendo que esse  ainda terá uma versão direcionada para mulheres, com mais 100 vagas disponíveis.",
+    postLink: "https://docs.google.com/forms/d/e/1FAIpQLSc9Q6G2A5yakCrGfUaqUFrIH63WYw4Tguo1MpOmcSittFHasQ/viewform",
+  },
+  {
+    cursoFilter: "back-end filter-dados",
+    imageLink: "src/assets/img/bootcamps/startbycapgemini.webp",
+    imageAlt: "Programa Start by Capgemini",
+    postTitle: "Programa Start by Capgemini",
+    postContent:
+      "O Programa START da Capgemini foi criado para ajudar quem já é da área de TI ou quem deseja mudar de carreira, a ingressar no mercado e conquistar independência financeira para realizar seus sonhos, independentemente da sua idade.\n\nVocê escolhe sua trilha nas principais plataformas e ainda tem a possibilidade de contratação no final do treinamento! ",
+    postLink: "https://startcapgemini.com.br",
+  },
+  {
+    cursoFilter: "back-end filter-marketing",
+    imageLink: "src/assets/img/bootcamps/institutodaoportunidadesocial.webp",
+    imageAlt: "Instituto da Oportunidade Social (IOS)",
+    postOptional: "Inscrições por tempo limitado!",
+    postTitle: "Instituto da Oportunidade Social (IOS)",
+    postContent:
+      "O Instituto da Oportunidade Social (IOS) abriu as inscrições para o segundo semestre de cursos de tecnologia presenciais e online!\n\nTem curso de Programação Web, Gestão empresarial com software ERP, Suporte em TI e muitos outros!",
+    postLink: "https://ios.org.br/inscricao",
+  },
+];
