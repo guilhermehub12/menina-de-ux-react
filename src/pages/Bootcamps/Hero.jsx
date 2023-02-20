f97671225f16b78a663a909c68729ed6abb6cddd
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-19 23:01:34 -0300

    build: Hero page to Bootcamps section

---
+++ src/pages/Bootcamps/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section id="hero" className="hero">
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-center">
+            <div data-aos="zoom-out">
+              <h1>Cursos Grandes e Bootcamps</h1>
+              <p>
+                As melhores e maiores certificações para quem quer entrar na
+                área e se destacar
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
