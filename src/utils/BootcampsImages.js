c1d0477176ef68c555f18beb91c89b053f337c9f
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-20 22:24:16 -0300

    Bootcamps images imported from bootcamps folder

---
+++ src/utils/BootcampsImages.js
@@ -0,0 +1,35 @@
+import bootcampinhouiux from "../assets/img/bootcamps/bootcampinhouiux.webp";
+import collabdesign from "../assets/img/bootcamps/collabdesign.webp";
+import cursosprefeituradebh from "../assets/img/bootcamps/cursosprefeituradebh.webp";
+import dataexbootcamp from "../assets/img/bootcamps/dataexbootcamp.webp";
+import decisaodecarreiranapratica from "../assets/img/bootcamps/decisaodecarreiranapratica.webp";
+import efenglishlive from "../assets/img/bootcamps/efenglishlive.webp";
+import gamaacademy from "../assets/img/bootcamps/gamaacademy.webp";
+import geracaocrescer from "../assets/img/bootcamps/geracaocrescer.webp";
+import googleeciee from "../assets/img/bootcamps/googleeciee.webp";
+import institutodaoportunidadesocial from "../assets/img/bootcamps/institutodaoportunidadesocial.webp";
+import institutoproa from "../assets/img/bootcamps/institutoproa.webp";
+import janelasparaoamanha from "../assets/img/bootcamps/janelasparaoamanha.webp";
+import proztecnologia from "../assets/img/bootcamps/proztecnologia.webp";
+import soulbilingue from "../assets/img/bootcamps/soulbilingue.webp";
+import startbycapgemini from "../assets/img/bootcamps/startbycapgemini.webp";
+
+const BootcampsImages = {
+    bootcampinhouiux: bootcampinhouiux,
+    collabdesign: collabdesign,
+    cursosprefeituradebh: cursosprefeituradebh,
+    dataexbootcamp: dataexbootcamp,
+    decisaodecarreiranapratica: decisaodecarreiranapratica,
+    efenglishlive: efenglishlive,
+    gamaacademy: gamaacademy,
+    geracaocrescer: geracaocrescer,
+    googleeciee: googleeciee,
+    institutodaoportunidadesocial: institutodaoportunidadesocial,
+    institutoproa: institutoproa,
+    janelasparaoamanha: janelasparaoamanha,
+    proztecnologia: proztecnologia,
+    soulbilingue: soulbilingue,
+    startbycapgemini: startbycapgemini,
+};
+
+export default BootcampsImages;
