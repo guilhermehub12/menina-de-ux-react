f005c986568e1edfa3242ce723c691a949f6096b
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-20 22:23:48 -0300

    All images imported from img folder

---
+++ src/utils/AllImages.js
@@ -0,0 +1,133 @@
+import Aprovado from "../assets/img/aprovado.webp";
+import ArquivoUltraSecreto from "../assets/img/arquivo-ultra-secreto.webp";
+import Certificado from "../assets/img/certificado.webp";
+import CompartilhandoNasRedesSociais from "../assets/img/compartilhando-nas-redes-sociais.webp";
+import ConexoesLinkedin from "../assets/img/conexoes-linkedin.webp";
+import Curriculo from "../assets/img/curriculo.webp";
+import EmConstrucao from "../assets/img/em-construcao.webp";
+import EquipeDesigners from "../assets/img/equipe-designers.webp";
+import FazerUserFlow from "../assets/img/fazer-user-flow.webp";
+import GirlPower from "../assets/img/girl-power.webp";
+import HeaderLogo from "../assets/img/header-logo.webp";
+import HomemCadeiranteAcenando from "../assets/img/homem-cadeirante-acenando.webp";
+import HomemComCelularGigante from "../assets/img/homem-com-celular-gigante.webp";
+import HomemComCofrinho from "../assets/img/homem-com-cofrinho.webp";
+import HomemComMaletaMochilaJato from "../assets/img/homem-com-maleta-mochila-jato.webp";
+import HomemComVideoGame from "../assets/img/homem-com-videogame.webp";
+import HomemConversandoComMulher from "../assets/img/homem-conversando-com-mulher.webp";
+import HomemDeNegocios from "../assets/img/homem-de-negocios.webp";
+import CasalAnalisandoDados from "../assets/img/homem-e-mulher-analisando-dados.webp";
+import CasalJuntandoQuebraCabecas from "../assets/img/homem-e-mulher-juntando-quebra-cabecas.webp";
+import HomemEscrevendoEmLousa from "../assets/img/homem-escrevendo-em-lousa.webp";
+import HomemEscrevendo from "../assets/img/homem-escrevendo.webp";
+import HomemJogandoVideoGame from "../assets/img/homem-jogando-videogame.webp";
+import HomemMontandoQuebraCabeca from "../assets/img/homem-montando-quebra-cabeca.webp";
+import HomemPesquisando from "../assets/img/homem-pesquisando.webp";
+import HomemSegurandoSinalPositivo from "../assets/img/homem-segurando-sinal-positivo.webp";
+import HomemTrabalhando from "../assets/img/homem-trabalhando.webp";
+import InteligenciaArtificial from "../assets/img/inteligencia-artificial.webp";
+import MeninaAssistindoAula from "../assets/img/menina-assistindo-aula.webp";
+import MeninoCriativoPensando from "../assets/img/menino-criativo-pensando.webp";
+import MulherAcenando2 from "../assets/img/mulher-acenando-2.webp";
+import MulherAcenando from "../assets/img/mulher-acenando.webp";
+import MulherAnunciandoRedesSociais from "../assets/img/mulher-anunciando-redes-sociais.webp";
+import MulherColocandoCheckNoPapel from "../assets/img/mulher-colocando-check-no-papel.webp";
+import MulherComAMaoNaCabecaFrustrada from "../assets/img/mulher-com-a-mao-na-cabeca-frustrada.webp";
+import MulherComLapisComputador from "../assets/img/mulher-com-lapis-computador.webp";
+import MulherComPapeisECartaNoComputador from "../assets/img/mulher-com-papeis-e-carta-no-computador.webp";
+import MulherConversandoComHomem from "../assets/img/mulher-conversando-com-homem.webp";
+import MulherConversandoComOutra from "../assets/img/mulher-conversando-com-outra.webp";
+import MulherCriandoWireframes from "../assets/img/mulher-criando-wireframes.webp";
+import MulherDesigner from "../assets/img/mulher-designer.webp";
+import MulherEHomemTomandoNotas from "../assets/img/mulher-e-homem-tomando-notas.webp";
+import MulherEstudando2 from "../assets/img/mulher-estudando-2.webp";
+import MulherEstudando from "../assets/img/mulher-estudando.webp";
+import MulherInvestigando from "../assets/img/mulher-investigando.webp";
+import MulherLendo from "../assets/img/mulher-lendo.webp";
+import MulherMexendoCelular from "../assets/img/mulher-mexendo-celular.webp";
+import MulherMostrandoTela from "../assets/img/mulher-mostrando-tela.webp";
+import MulherNoComputadorComGato from "../assets/img/mulher-no-computador-com-gato.webp";
+import MulherNoComputador from "../assets/img/mulher-no-computador.webp";
+import MulherPensando from "../assets/img/mulher-pensando.webp";
+import MulherRecebendoNovidades from "../assets/img/mulher-recebendo-novidades.webp";
+import MulherRedesSociais from "../assets/img/mulher-redes-sociais.webp";
+import Nfts from "../assets/img/nfts.webp";
+import Ods4 from "../assets/img/ods-4.webp";
+import Ods5 from "../assets/img/ods-5.webp";
+import PessoaSemRumo from "../assets/img/pessoa-sem-rumo.webp";
+import PessoasTrabalhandoJuntas from "../assets/img/pessoas-trabalhando-juntas.webp";
+import PreenchendoNotas from "../assets/img/preenchendo-notas.webp";
+import PreenchendoPostIts from "../assets/img/preenchendo-post-its.webp";
+import ProcessosSeletivos from "../assets/img/processos-seletivos.webp";
+import SaudeMentalNoTrabalho from "../assets/img/saude-mental-no-trabalho.webp";
+import TimeSegurandoBandeira from "../assets/img/time-segurando-bandeira.webp";
+import Vencedores from "../assets/img/vencedores.webp";
+
+const AllImages = {
+  MulherEstudando: MulherEstudando,
+  MulherEstudando2: MulherEstudando2,
+  MulherAcenando: MulherAcenando,
+  MulherLendo: MulherLendo,
+  Aprovado: Aprovado,
+  ArquivoUltraSecreto: ArquivoUltraSecreto,
+  Certificado: Certificado,
+  CompartilhandoNasRedesSociais: CompartilhandoNasRedesSociais,
+  ConexoesLinkedin: ConexoesLinkedin,
+  Curriculo: Curriculo,
+  EmConstrucao: EmConstrucao,
+  EquipeDesigners: EquipeDesigners,
+  FazerUserFlow: FazerUserFlow,
+  GirlPower: GirlPower,
+  HeaderLogo: HeaderLogo,
+  HomemCadeiranteAcenando: HomemCadeiranteAcenando,
+  HomemComCelularGigante: HomemComCelularGigante,
+  HomemComCofrinho: HomemComCofrinho,
+  HomemComMaletaMochilaJato: HomemComMaletaMochilaJato,
+  HomemComVideoGame: HomemComVideoGame,
+  HomemConversandoComMulher: HomemConversandoComMulher,
+  HomemDeNegocios: HomemDeNegocios,
+  CasalAnalisandoDados: CasalAnalisandoDados,
+  CasalJuntandoQuebraCabecas: CasalJuntandoQuebraCabecas,
+  HomemEscrevendoEmLousa: HomemEscrevendoEmLousa,
+  HomemEscrevendo: HomemEscrevendo,
+  HomemJogandoVideoGame: HomemJogandoVideoGame,
+  HomemMontandoQuebraCabeca: HomemMontandoQuebraCabeca,
+  HomemPesquisando: HomemPesquisando,
+  HomemSegurandoSinalPositivo: HomemSegurandoSinalPositivo,
+  HomemTrabalhando: HomemTrabalhando,
+  InteligenciaArtificial: InteligenciaArtificial,
+  MeninaAssistindoAula: MeninaAssistindoAula,
+  MeninoCriativoPensando: MeninoCriativoPensando,
+  MulherAcenando2: MulherAcenando2,
+  MulherAnunciandoRedesSociais: MulherAnunciandoRedesSociais,
+  MulherColocandoCheckNoPapel: MulherColocandoCheckNoPapel,
+  MulherComAMaoNaCabecaFrustrada: MulherComAMaoNaCabecaFrustrada,
+  MulherComLapisComputador: MulherComLapisComputador,
+  MulherComPapeisECartaNoComputador: MulherComPapeisECartaNoComputador,
+  MulherConversandoComHomem: MulherConversandoComHomem,
+  MulherConversandoComOutra: MulherConversandoComOutra,
+  MulherCriandoWireframes: MulherCriandoWireframes,
+  MulherDesigner: MulherDesigner,
+  MulherEHomemTomandoNotas: MulherEHomemTomandoNotas,
+  MulherInvestigando: MulherInvestigando,
+  MulherMexendoCelular: MulherMexendoCelular,
+  MulherMostrandoTela: MulherMostrandoTela,
+  MulherNoComputadorComGato: MulherNoComputadorComGato,
+  MulherNoComputador: MulherNoComputador,
+  MulherPensando: MulherPensando,
+  MulherRecebendoNovidades: MulherRecebendoNovidades,
+  MulherRedesSociais: MulherRedesSociais,
+  Nfts: Nfts,
+  Ods4: Ods4,
+  Ods5: Ods5,
+  PessoaSemRumo: PessoaSemRumo,
+  PessoasTrabalhandoJuntas: PessoasTrabalhandoJuntas,
+  PreenchendoNotas: PreenchendoNotas,
+  PreenchendoPostIts: PreenchendoPostIts,
+  ProcessosSeletivos: ProcessosSeletivos,
+  SaudeMentalNoTrabalho: SaudeMentalNoTrabalho,
+  TimeSegurandoBandeira: TimeSegurandoBandeira,
+  Vencedores: Vencedores,
+};
+
+export default AllImages;
