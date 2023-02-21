64639f883e1d42b64432c8487b0b8091aac0c960
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-20 22:25:26 -0300

    Bootcamps posts to bootcamps page

--- src/utils/BootcampsItems.js
+++ src/utils/BootcampsItems.js
@@ -1,16 +1,18 @@
+import BootcampsImages from "./BootcampsImages";
+
 export const BootcampsItems = [
   {
     cursoFilter: "design",
-    imageLink: "src/assets/img/bootcamps/bootcampinhouiux.webp",
+    imageLink: { imagem: BootcampsImages.bootcampinhouiux },
     imageAlt: "Bootcampinho UI/UX",
     postTitle: "Bootcampinho UI/UX",
     postContent:
       "O Bootcampinho Gratuito UI/UX Design tem como objetivo te ajudar nos primeiros passos de sua jornada. Formulado com o objetivo de ser um ponto de partida o curso é dividido entre etapas teóricas e práticas. \n\nAlém disso, você será desafiado(a) durante todo o curso a criar um projeto de UI/UX do início ao fim. Todas as aulas também contam com desafios e materiais de para ajudar você a continuar seus estudos.",
-      postLink: "https://maiane.com.br/bootcampinho-ui-ux-design/",
+    postLink: "https://maiane.com.br/bootcampinho-ui-ux-design/",
   },
   {
     cursoFilter: "back-end",
-    imageLink: "src/assets/img/bootcamps/proztecnologia.webp",
+    imageLink: { imagem: BootcampsImages.proztecnologia },
     imageAlt: "Capacitação Gratuita em Introdução à Programação",
     postTitle: "Capacitação Gratuita em Introdução à Programação",
     postOptional: "Inscrições por tempo limitado!",
@@ -20,7 +22,7 @@ export const BootcampsItems = [
   },
   {
     cursoFilter: "active",
-    imageLink: "src/assets/img/bootcamps/gamaacademy.webp",
+    imageLink: { imagem: BootcampsImages.gamaacademy },
     imageAlt: "6 cursos grátis da Gama Academy",
     postTitle: "6 cursos grátis da Gama Academy",
     postContent:
@@ -29,16 +31,17 @@ export const BootcampsItems = [
   },
   {
     cursoFilter: "active",
-    imageLink: "src/assets/img/bootcamps/cursosprefeituradebh.webp",
+    imageLink: { imagem: BootcampsImages.cursosprefeituradebh },
     imageAlt: "Cursos de TI da Prefeitura de BH",
     postTitle: "Cursos de TI da Prefeitura de BH",
     postContent:
       "A Prefeitura de Belo Horizonte está com mais de 2.500 vagas disponíveis para cursos de inclusão digital nos formatos presencial e EAD para a área de tecnologia da informação. \n\nSão cinco cursos disponíveis, cada um deles com 500 vagas. O aluno pode cursar todos eles, iniciando com Introdução às Novas Tecnologias, Planilha Eletrônica, Ferramentas para Home Office, chegando à Programação Web, sendo que esse  ainda terá uma versão direcionada para mulheres, com mais 100 vagas disponíveis.",
-    postLink: "https://docs.google.com/forms/d/e/1FAIpQLSc9Q6G2A5yakCrGfUaqUFrIH63WYw4Tguo1MpOmcSittFHasQ/viewform",
+    postLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc9Q6G2A5yakCrGfUaqUFrIH63WYw4Tguo1MpOmcSittFHasQ/viewform",
   },
   {
     cursoFilter: "back-end filter-dados",
-    imageLink: "src/assets/img/bootcamps/startbycapgemini.webp",
+    imageLink: { imagem: BootcampsImages.startbycapgemini },
     imageAlt: "Programa Start by Capgemini",
     postTitle: "Programa Start by Capgemini",
     postContent:
@@ -47,12 +50,62 @@ export const BootcampsItems = [
   },
   {
     cursoFilter: "back-end filter-marketing",
-    imageLink: "src/assets/img/bootcamps/institutodaoportunidadesocial.webp",
+    imageLink: { imagem: BootcampsImages.institutodaoportunidadesocial },
     imageAlt: "Instituto da Oportunidade Social (IOS)",
     postOptional: "Inscrições por tempo limitado!",
     postTitle: "Instituto da Oportunidade Social (IOS)",
     postContent:
       "O Instituto da Oportunidade Social (IOS) abriu as inscrições para o segundo semestre de cursos de tecnologia presenciais e online!\n\nTem curso de Programação Web, Gestão empresarial com software ERP, Suporte em TI e muitos outros!",
     postLink: "https://ios.org.br/inscricao",
+  },
+  {
+    cursoFilter: "mulheres",
+    imageLink: { imagem: BootcampsImages.decisaodecarreiranapratica },
+    imageAlt: "Decisão de Carreira na Prática",
+    postOptional: "Gratuito por tempo limitado!",
+    postTitle: "Decisão de Carreira na Prática",
+    postContent:
+      "A Fundação Estudar liberou gratuitamente por tempo limitado o curso de Decisão de Carreira na Prática, feito para quem quer aprofundar conhecimento em sua trajetória e entenda como seus interesses, habilidades e valores podem direcionar as suas decisões profissionais!",
+    postLink: "https://conteudos.napratica.org.br/decisao-de-carreira-na-pratica/",
+  },
+  {
+    cursoFilter: "design",
+    imageLink: { imagem: BootcampsImages.collabdesign },
+    imageAlt: "collabdesign",
+    postTitle: "collabdesign",
+    postContent: "A Collab Design está oferecendo um curso onde você vai entender o que é ser designer, sua rotina e o mercado de trabalho! \n\nVocê vai aprender o essencial para criar qualquer tipo de arte gráfica, seja post de redes sociais, materiais impressos e logotipos.",
+    postLink: "https://www.sympla.com.br/play/design-e-para-todos-curso-intensivo-de-design-visual-grafico/1090448?share_id=0",
+  },
+  {
+    cursoFilter: "jovens filter-dados filter-design",
+    imageLink: { imagem: BootcampsImages.googleeciee },
+    imageAlt: "Bolsas de Estudos Google",
+    postTitle: "Bolsas de Estudos Google",
+    postContent: "O CIEE está com as inscrições abertas para um processo seletivo de bolsas de estudos para cursos 100% on-line que contam com um conteúdo altamente interativo e uma certificação profissional. \n\nSão 30 mil bolsas de estudos e você pode estudar no seu ritmo e de onde quiser!",
+    postLink: "https://portal.ciee.org.br/para-voce/google-ciee",
+  },
+  {
+    cursoFilter: "jovens",
+    imageLink: { imagem: BootcampsImages.geracaocrescer },
+    imageAlt: "Geração Crescer",
+    postTitle: "Geração Crescer",
+    postContent: "O projeto Geração Crescer está com 34 mil para cursos gratuitos. \n\nO ensino é 100% online e se direciona para os jovens com mais de 16 anos, que procuram novas oportunidades de profissionalização e capacitação.",
+    postLink: "https://geracaocrescer.org.br/web/",
+  },
+  {
+    cursoFilter: "jovens",
+    imageLink: { imagem: BootcampsImages.janelasparaoamanha },
+    imageAlt: "Janelas para o Amanhã",
+    postTitle: "Janelas para o Amanhã",
+    postContent: "O projeto Janelas para o Amanhã é uma parceria da Recode com a Petrobras nos estados do RJ, SP e ES. \n\nNa plataforma, oferecem formações gratuitas em tecnologia para jovens do ensino médio e membros das comunidades escolares.",
+    postLink: "https://janelas.recode.org.br",
+  },
+  {
+    cursoFilter: "jovens",
+    imageLink: { imagem: BootcampsImages.institutoproa },
+    imageAlt: "Instituto PROA",
+    postTitle: "Instituto PROA",
+    postContent: "O Instituto PROA nasceu com o objetivo de criar oportunidades de desenvolvimento pessoal e profissional para jovens de baixa renda vindos de escola pública.",
+    postLink: "https://www.proa.org.br",
   },
 ];
