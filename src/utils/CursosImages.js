f953f39d7fc81ad1f6bb7c33c6eb931e0a14fc87
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-20 22:24:34 -0300

    Cursos images imported from cursos folder

---
+++ src/utils/CursosImages.js
@@ -0,0 +1,145 @@
+import MIO from "../assets/img/cursos/1mio.webp";
+import adobeeduca from "../assets/img/cursos/adobeeduca.webp";
+import amazonalexa from "../assets/img/cursos/amazonalexa.webp";
+import avamec from "../assets/img/cursos/avamec.webp";
+import awari from "../assets/img/cursos/awari.webp";
+import awsskillbuilder from "../assets/img/cursos/awsskillbuilder.webp";
+import bancocentraldobrasil from "../assets/img/cursos/bancocentraldobrasil.webp";
+import bolsagratis from "../assets/img/cursos/bolsagratis.webp";
+import bradesco from "../assets/img/cursos/bradesco.webp";
+import cieesabervirtual from "../assets/img/cursos/cieesabervirtual.webp";
+import coliga from "../assets/img/cursos/coliga.webp";
+import coursera from "../assets/img/cursos/coursera.webp";
+import crescacomogoogle from "../assets/img/cursos/crescacomogoogle.webp";
+import cursae from "../assets/img/cursos/cursae.webp";
+import cursoemvideo from "../assets/img/cursos/cursoemvideo.webp";
+import datascienceacademy from "../assets/img/cursos/datascienceacademy.webp";
+import digitalods from "../assets/img/cursos/digitalods.webp";
+import dio from "../assets/img/cursos/dio.webp";
+import educacaolivre from "../assets/img/cursos/educacaolivre.webp";
+import edx from "../assets/img/cursos/edx.webp";
+import escoladanuvem from "../assets/img/cursos/escoladanuvem.webp";
+import escoladotrabalhador from "../assets/img/cursos/escoladotrabalhador.webp";
+import escolavirtualgov from "../assets/img/cursos/escolavirtualgov.webp";
+import estacaohack from "../assets/img/cursos/estacaohack.webp";
+import estudarfora from "../assets/img/cursos/estudarfora.webp";
+import eucapacito from "../assets/img/cursos/eucapacito.webp";
+import fecap from "../assets/img/cursos/fecap.webp";
+import fgv from "../assets/img/cursos/fgv.webp";
+import fiap from "../assets/img/cursos/fiap.webp";
+import ginead from "../assets/img/cursos/ginead.webp";
+import googleskillshop from "../assets/img/cursos/googleskillshop.webp";
+import hiacademy from "../assets/img/cursos/hiacademy.webp";
+import houseacademy from "../assets/img/cursos/houseacademy.webp";
+import ifrs from "../assets/img/cursos/ifrs.webp";
+import inspiraja from "../assets/img/cursos/inspiraja.webp";
+import institutotie from "../assets/img/cursos/institutotie.webp";
+import ituring from "../assets/img/cursos/ituring.webp";
+import ivyleague from "../assets/img/cursos/ivyleague.webp";
+import kultivi from "../assets/img/cursos/kultivi.webp";
+import linkedinpremium from "../assets/img/cursos/linkedinpremium.webp";
+import lumina from "../assets/img/cursos/lumina.webp";
+import maisprati from "../assets/img/cursos/maisprati.webp";
+import metablueprint from "../assets/img/cursos/metablueprint.webp";
+import microlins from "../assets/img/cursos/microlins.webp";
+import microsoftlearn from "../assets/img/cursos/microsoftlearn.webp";
+import mundosenai from "../assets/img/cursos/mundosenai.webp";
+import navedoconhecimento from "../assets/img/cursos/navedoconhecimento.webp";
+import nicbr from "../assets/img/cursos/nicbr.webp";
+import poca from "../assets/img/cursos/poca.webp";
+import portaltech from "../assets/img/cursos/portaltech.webp";
+import potenciatech from "../assets/img/cursos/potenciatech.webp";
+import prageralver from "../assets/img/cursos/prageralver.webp";
+import prepposgraduacao from "../assets/img/cursos/prepposgraduacao.webp";
+import pucrs from "../assets/img/cursos/pucrs.webp";
+import punkmetrics from "../assets/img/cursos/punkmetrics.webp";
+import recode from "../assets/img/cursos/recode.webp";
+import rockuniversity from "../assets/img/cursos/rockuniversity.webp";
+import samsungocean from "../assets/img/cursos/samsungocean.webp";
+import sebrae from "../assets/img/cursos/sebrae.webp";
+import senai from "../assets/img/cursos/senai.webp";
+import senaiplay from "../assets/img/cursos/senaiplay.webp";
+import timtec from "../assets/img/cursos/timtec.webp";
+import tramplay from "../assets/img/cursos/tramplay.webp";
+import twitter from "../assets/img/cursos/twitter.webp";
+import udemy from "../assets/img/cursos/udemy.webp";
+import universidadesolides from "../assets/img/cursos/universidadesolides.webp";
+import unova from "../assets/img/cursos/unova.webp";
+import usp from "../assets/img/cursos/usp.webp";
+import uxwritinghub from "../assets/img/cursos/uxwritinghub.webp";
+import womakerscode from "../assets/img/cursos/womakerscode.webp";
+
+const CursosImages = {
+  MIO: MIO,
+  adobeeduca: adobeeduca,
+  amazonalexa: amazonalexa,
+  avamec: avamec,
+  awari: awari,
+  awsskillbuilder: awsskillbuilder,
+  bancocentraldobrasil: bancocentraldobrasil,
+  bolsagratis: bolsagratis,
+  bradesco: bradesco,
+  cieesabervirtual: cieesabervirtual,
+  coliga: coliga,
+  coursera: coursera,
+  crescacomogoogle: crescacomogoogle,
+  cursae: cursae,
+  cursoemvideo: cursoemvideo,
+  datascienceacademy: datascienceacademy,
+  digitalods: digitalods,
+  dio: dio,
+  educacaolivre: educacaolivre,
+  edx: edx,
+  escoladanuvem: escoladanuvem,
+  escoladotrabalhador: escoladotrabalhador,
+  escolavirtualgov: escolavirtualgov,
+  estacaohack: estacaohack,
+  estudarfora: estudarfora,
+  eucapacito: eucapacito,
+  fecap: fecap,
+  fgv: fgv,
+  fiap: fiap,
+  ginead: ginead,
+  googleskillshop: googleskillshop,
+  hiacademy: hiacademy,
+  houseacademy: houseacademy,
+  ifrs: ifrs,
+  inspiraja: inspiraja,
+  institutotie: institutotie,
+  ituring: ituring,
+  ivyleague: ivyleague,
+  kultivi: kultivi,
+  linkedinpremium: linkedinpremium,
+  lumina: lumina,
+  maisprati: maisprati,
+  metablueprint: metablueprint,
+  microlins: microlins,
+  microsoftlearn: microsoftlearn,
+  mundosenai: mundosenai,
+  navedoconhecimento: navedoconhecimento,
+  nicbr: nicbr,
+  poca: poca,
+  portaltech: portaltech,
+  potenciatech: potenciatech,
+  prageralver: prageralver,
+  prepposgraduacao: prepposgraduacao,
+  pucrs: pucrs,
+  punkmetrics: punkmetrics,
+  recode: recode,
+  rockuniversity: rockuniversity,
+  samsungocean: samsungocean,
+  sebrae: sebrae,
+  senai: senai,
+  senaiplay: senaiplay,
+  timtec: timtec,
+  tramplay: tramplay,
+  twitter: twitter,
+  udemy: udemy,
+  universidadesolides: universidadesolides,
+  unova: unova,
+  usp: usp,
+  uxwritinghub: uxwritinghub,
+  womakerscode: womakerscode,
+};
+
+export default CursosImages;
