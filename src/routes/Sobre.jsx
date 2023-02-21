73f62ab5b94851b442c45a62c816f571140ce273
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-20 22:21:50 -0300

    routes: Sobre route page

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import Hero from "../pages/Sobre/Hero.jsx";
+import SobreSection from "../pages/Sobre/SobreSection.jsx";
+import ObjetivosSection from "../pages/Sobre/ObjetivosSection.jsx";
 
 function Sobre() {
   return (
     <>
       <Header />
       <Hero />
+      <SobreSection />
+      <ObjetivosSection />
       <Footer />
     </>
   );
