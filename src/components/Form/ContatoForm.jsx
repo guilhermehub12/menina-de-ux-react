eb0761818c699554689dad7ea6963cbcba6f9974
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-22 01:59:15 -0300

    feat: contact form with formik and yup

---
+++ src/components/Form/ContatoForm.jsx
@@ -0,0 +1,102 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function ContatoForm() {
+  const initialValues = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Por favor, informe seu nome"),
+    email: Yup.string()
+      .email("Por favor, informe um endereço de email válido")
+      .required("Por favor, informe seu endereço de email"),
+    message: Yup.string().required("Por favor, escreva uma mensagem"),
+  });
+
+  const onSubmit = async (values, { resetForm }) => {
+    const response = await fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(values).toString(),
+    });
+    if (response.status === 200) {
+      resetForm();
+      alert("Obrigado pelo seu contato! Responderemos em breve.");
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ errors, touched }) => (
+        <Form name="contato" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contato" />
+          <div className="form-group">
+            <label htmlFor="name">Nome</label>
+            <Field
+              className={`form-control ${
+                errors.name && touched.name ? "is-invalid" : ""
+              }`}
+              name="name"
+              id="name"
+              placeholder="Digite seu nome"
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <Field
+              className={`form-control ${
+                errors.email && touched.email ? "is-invalid" : ""
+              }`}
+              name="email"
+              id="email"
+              placeholder="Digite seu email"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Mensagem</label>
+            <Field
+              className={`form-control ${
+                errors.message && touched.message ? "is-invalid" : ""
+              }`}
+              name="message"
+              id="message"
+              as="textarea"
+              rows={5}
+              placeholder="Digite sua mensagem"
+            />
+            <ErrorMessage
+              name="message"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Enviar
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default ContatoForm;
--- src/routes/Contatos.jsx
+++ src/routes/Contatos.jsx
@@ -3,12 +3,14 @@ import Header from "../components/Header/Header.jsx";
 import Telegram from "../components/Telegram/Telegram.jsx";
 import Contato from "../components/Contato/Contato.jsx";
 import Footer from "../components/Footer/Footer.jsx";
+import ContatoForm from "../components/Form/ContatoForm.jsx";
 
 function Contatos() {
   return (
     <>
       <Header />
       <Telegram />
+      <ContatoForm />
       <Contato />
       <Footer />
     </>
