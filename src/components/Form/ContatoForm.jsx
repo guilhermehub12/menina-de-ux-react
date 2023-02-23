d5ab8d3c452dbeb1de7c2bd86b87aac8f2be6707
guilhermehub12 <guilhermee1312@hotmail.com>
2023-02-22 21:32:07 -0300

    feat: Contato form to send emails

--- src/components/Form/ContatoForm.jsx
+++ src/components/Form/ContatoForm.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useForm } from "@formspree/react";
+import { Modal, Button, FormLabel } from "react-bootstrap";
+import "./ContatoForm.css";
 
 function ContatoForm() {
   const initialValues = {
@@ -16,81 +20,109 @@ function ContatoForm() {
     message: Yup.string().required("Por favor, escreva uma mensagem"),
   });
 
-  const onSubmit = async ({ resetForm }) => {
-    setTimeout(() => {
-      resetForm();
-      alert("Obrigado pelo seu contato! Responderemos em breve.");
-    }, 400);
+  const [state, handleSubmit] = useForm("xdovgyay");
+  const [showModal, setShowModal] = useState(false);
+
+  const handleFormSubmit = (values, actions) => {
+    handleSubmit(values);
+    setShowModal(true);
+    actions.setSubmitting(false);
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      {({ errors, touched }) => (
-        <Form name="contato" method="POST" netlify >
-          <input type="hidden" name="form-name" value="contato" />
-          <div className="form-group">
-            <label htmlFor="name">Nome</label>
-            <Field
-              className={`form-control ${
-                errors.name && touched.name ? "is-invalid" : ""
-              }`}
-              name="name"
-              id="name"
-              placeholder="Digite seu nome"
-            />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className="invalid-feedback"
-            />
-          </div>
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleFormSubmit}
+      >
+        {({ errors, touched }) => (
+          <Form
+            className="contact-form"
+            name="contato"
+            method="POST"
+            data-netlify="true"
+          >
+            <h1>Contate-nos</h1>
+            <input type="hidden" name="form-name" value="contato" />
+            <div className="form-group">
+              <FormLabel htmlFor="name">Nome</FormLabel>
+              <Field
+                className={`form-control ${
+                  errors.name && touched.name ? "is-invalid" : ""
+                }`}
+                name="name"
+                id="name"
+                placeholder="Digite seu nome"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field
-              className={`form-control ${
-                errors.email && touched.email ? "is-invalid" : ""
-              }`}
-              name="email"
-              id="email"
-              placeholder="Digite seu email"
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="invalid-feedback"
-            />
-          </div>
+            <div className="form-group">
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Field
+                className={`form-control ${
+                  errors.email && touched.email ? "is-invalid" : ""
+                }`}
+                name="email"
+                id="email"
+                placeholder="Digite seu email"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="message">Mensagem</label>
-            <Field
-              className={`form-control ${
-                errors.message && touched.message ? "is-invalid" : ""
-              }`}
-              name="message"
-              id="message"
-              as="textarea"
-              rows={5}
-              placeholder="Digite sua mensagem"
-            />
-            <ErrorMessage
-              name="message"
-              component="div"
-              className="invalid-feedback"
-            />
-          </div>
-
-          <button type="submit" className="btn btn-primary">
-            Enviar
-          </button>
-        </Form>
-      )}
-    </Formik>
+            <div className="form-group">
+              <FormLabel htmlFor="message">Mensagem</FormLabel>
+              <Field
+                className={`form-control ${
+                  errors.message && touched.message ? "is-invalid" : ""
+                }`}
+                name="message"
+                id="message"
+                as="textarea"
+                rows={4}
+                placeholder="Digite sua mensagem"
+              />
+              <ErrorMessage
+                component="div"
+                name="message"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <Button type="submit" variant="primary">
+                Enviar
+              </Button>
+            </div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  Mensagem de {state.succeeded ? "sucesso" : "erro"}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {state.succeeded
+                  ? "Sua mensagem foi enviada com sucesso!"
+                  : "Houve um erro ao enviar a mensagem. Tente novamente mais tarde."}
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShowModal(false)}>
+                  Fechar
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 }
 
--- src/routes/Contatos.jsx
+++ src/routes/Contatos.jsx
@@ -10,8 +10,8 @@ function Contatos() {
     <>
       <Header />
       <Telegram />
-      <ContatoForm />
       <Contato />
+      <ContatoForm />
       <Footer />
     </>
   );
